b74017204b4e2d34a9b7bedc2fb64c0fe731db77
Nurkaiyr <yedige21@gmail.com>
2017-12-27 00:21:27 +0600

    practice

---
+++ IndexatorPractice/RangeOfArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorPractice
+{
+    public class RangeOfArray
+    {
+        private object[] array;
+        private int size;
+        private int start;
+        private int end;
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public int Start
+        {
+            get { return start; }
+            set { start = value; }
+        }
+
+        public int End
+        {
+            get { return end; }
+            set { end = value; }
+        }
+
+        public RangeOfArray()
+        {
+            Size = 0;
+            array = new object[Size];
+        }
+
+        public RangeOfArray(int start, int end)
+        {
+            Start = start;
+            End = end;
+            Size = end - start + 1;
+            array = new object[Size];
+        }
+
+        public object this[int i]
+        {
+            get { return array[Math.Abs(i) - Math.Abs(Start)]; }
+            set { array[Math.Abs(i) - Math.Abs(Start)] = value;}
+        }
+
+    }
+}
